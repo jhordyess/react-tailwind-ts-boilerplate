c3662d06c2e8f16aa4c2deb1b0c3b072a7af7f25
jhordyess <dev@jhordyess.com>
2025-04-22 21:30:32 -0400

    Update vite.config.ts to adjust base path comment for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-tailwind-ts-boilerplate/',
-  resolve: { alias: { '@': '/src' } }
+  resolve: { alias: { '@': '/src' } },
+  // base: '/react-tailwind-ts-starter/', // Uncomment this line if deploying to GitHub Pages
 })
